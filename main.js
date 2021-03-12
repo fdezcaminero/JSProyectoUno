9ebbd667baa56b6db3546e0e3d852b745428f8dd
Jose <fdezcaminero@gmail.com>
2021-03-12 17:38:05 -0400

    I had been working on this but forgot to commit / this is first commit / I have the 'next' and 'previous' buttons working

---
+++ main.js
@@ -0,0 +1,93 @@
+let myLibrary = [];
+
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+var TSAR = new Book("The Sun Also Rises", "Ernest Hemingway", "500", "Read");
+
+var Gatsby = new Book("The Great Gatsby", "Fitzgerald", "300", "Not Read");
+
+var Junot = new Book("The Brief Wondrous Life of Oscar Wao", "Junot Díaz", "400", "Read");
+
+var Watchmen = new Book("Watchmen", "Alan Moore", "540", "Read");
+
+
+function addBookToLibrary(crazyBook) {
+    myLibrary.push(crazyBook);
+}
+
+addBookToLibrary(TSAR);
+addBookToLibrary(Gatsby);
+addBookToLibrary(Junot);
+addBookToLibrary(Watchmen);
+
+let index = 0;
+
+const crazyTitle = document.querySelector('#crazyTitle');
+crazyTitle.textContent = myLibrary[0].title;
+
+const crazyAuthor = document.querySelector('#crazyAuthor');
+crazyAuthor.textContent = myLibrary[0].author;
+
+const crazyPages = document.querySelector('#crazyPages');
+crazyPages.textContent = myLibrary[0].pages;
+
+const crazyRead = document.querySelector('#crazyRead');
+crazyRead.textContent = myLibrary[0].read;
+
+
+function nextBook() {
+    if(index >= (myLibrary.length - 1)) {
+        index = 0;
+    } else {
+        index++;
+    }
+
+    const crazyTitle = document.querySelector('#crazyTitle');
+    crazyTitle.textContent = myLibrary[index].title;
+
+    const crazyAuthor = document.querySelector('#crazyAuthor');
+    crazyAuthor.textContent = myLibrary[index].author;
+
+    const crazyPages = document.querySelector('#crazyPages');
+    crazyPages.textContent = myLibrary[index].pages;
+
+    const crazyRead = document.querySelector('#crazyRead');
+    crazyRead.textContent = myLibrary[index].read;
+}
+
+function previousBook() {
+    if(index === 0) {
+        index = (myLibrary.length - 1);
+    } else {
+        index--;
+    }
+
+    const crazyTitle = document.querySelector('#crazyTitle');
+    crazyTitle.textContent = myLibrary[index].title;
+
+    const crazyAuthor = document.querySelector('#crazyAuthor');
+    crazyAuthor.textContent = myLibrary[index].author;
+
+    const crazyPages = document.querySelector('#crazyPages');
+    crazyPages.textContent = myLibrary[index].pages;
+
+    const crazyRead = document.querySelector('#crazyRead');
+    crazyRead.textContent = myLibrary[index].read;
+}
+
+function removeBook() {
+
+}
+
+/*
+PrintStuff.prototype.print = function () {
+    console.log(this.documents);
+    }
+
+*/
